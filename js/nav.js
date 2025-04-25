b606ad59aff6e25404e88fe588774cbba3fb4107
DEVR01001 <rafael.dev01001@gmail.com>
2025-04-25 16:34:43 -0400

    resposividade

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+let BtnOpen = document.getElementById('btn-open')
+
+let ConteudoOpen = document.getElementById('cont-open')
+
+
+
+
+BtnOpen.addEventListener('click', () =>{
+
+    ConteudoOpen.classList.toggle('active_nav')
+
+    
+})
---
+++ js/voltar.js
@@ -0,0 +1,22 @@
+const btnVoltar = document.getElementById('voltar')
+
+btnVoltar.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+        
+
+    })
+})
+
+
+const btnTec = document.getElementById('tec1')
+
+btnTec.addEventListener('click', ()=>{
+    window.scrollTo({
+        Top: 0,
+        behavior: 'smooth'
+        
+
+    })
+})
